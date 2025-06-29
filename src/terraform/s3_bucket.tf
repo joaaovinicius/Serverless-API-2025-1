def9a546885dd5d6da91c0fa70fdd4bcc2ca8808
lucas-bruzzone-ectare <lucas.bruzzone@ectare.com.br>
2025-06-29 16:16:30 -0300

    Update S3 bucket name to include student ID for uniqueness

--- src/terraform/s3_bucket.tf
+++ src/terraform/s3_bucket.tf
@@ -1,4 +1,8 @@
 # S3 Bucket
 resource "aws_s3_bucket" "main_bucket" {
-  bucket = "${var.project_name}-bucket"
+  bucket = "${var.project_name}-${var.student_id}-bucket"
+}
+
+variable "student_id" {
+  default = "lucas-bruzzone"
 }
