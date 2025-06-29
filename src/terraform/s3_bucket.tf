dd3d35dcd833dd3ff85841afe4836fe195a93e64
lucas-bruzzone-ectare <lucas.bruzzone@ectare.com.br>
2025-06-29 16:00:28 -0300

    Remove random string suffix from S3 bucket name in Terraform configuration

--- src/terraform/s3_bucket.tf
+++ src/terraform/s3_bucket.tf
@@ -1,10 +1,4 @@
 # S3 Bucket
 resource "aws_s3_bucket" "main_bucket" {
-  bucket = "${var.project_name}-bucket-${random_string.suffix.result}"
-}
-
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-  upper   = false
+  bucket = "${var.project_name}-bucket"
 }
