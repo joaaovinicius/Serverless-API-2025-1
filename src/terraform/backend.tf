8774fa9b51179babc67635d9308d9d163253533b
lucas-bruzzone-ectare <lucas.bruzzone@ectare.com.br>
2025-06-29 16:13:09 -0300

    Update S3 bucket name in Terraform backend configuration to reflect the 2025 version

--- src/terraform/backend.tf
+++ src/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-aws-academy-2024"
+    bucket = "terraform-state-aws-academy-2025"
     key    = "academy-lab/terraform.tfstate"
     region = "us-east-1"
   }
