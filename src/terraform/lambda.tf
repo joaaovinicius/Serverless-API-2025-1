b1399be6070e1df55deb883e85b26f428707c9b1
lucas-bruzzone-ectare <lucas.bruzzone@ectare.com.br>
2025-06-29 16:23:07 -0300

    Add dynamic account ID retrieval to lambda.tf

--- src/terraform/lambda.tf
+++ src/terraform/lambda.tf
@@ -26,3 +26,6 @@ resource "aws_cloudwatch_log_group" "lambda_log_group" {
   name              = "/aws/lambda/${var.project_name}-lambda"
   retention_in_days = 14
 }
+
+# Obter account ID dinamicamente
+data "aws_caller_identity" "current" {}
