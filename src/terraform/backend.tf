6492e43071024ad322e50b63a648164747493f8b
joaaovinicius <110140140+joaaovinicius@users.noreply.github.com>
2025-06-30 21:24:37 -0300

    Update backend.tf

--- src/terraform/backend.tf
+++ src/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-aws-academy-2025"
+    bucket = "terraform-state-aws-academy-2025-libertas-${var.student_id}"
     key    = "academy-lab/terraform.tfstate"
     region = "us-east-1"
   }
